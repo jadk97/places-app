9828fea9c9f77d48e7564fec20883ed0adb1e13d
Jad K <jad_dis@hotmail.com>
2020-02-13 02:28:41 -0500

    created UsersList and UsersItem components with some placeholder data to render users with

--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -1,7 +1,17 @@
 import React from "react";
+import UsersList from "../components/UsersList";
 
-const Users= () => {
-  return <h2>Users page</h2>
+const Users = () => {
+  const USERS = [
+    {
+      id: "u1",
+      name: "John Doe",
+      image: "https://randomuser.me/api/portraits/men/90.jpg",
+      places: 4
+    }
+  ];
+
+  return <UsersList items={USERS} />
 };
 
 export default Users;
