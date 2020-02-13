7704efa2aff442f775376bf581de82483cba6f35
Jad K <jad_dis@hotmail.com>
2020-02-13 04:43:30 -0500

    implemented Places filtering based on user and added created a reusable button component

--- src/places/pages/UserPlaces.js
+++ src/places/pages/UserPlaces.js
@@ -1,19 +1,19 @@
 import React from "react";
-
+import { useParams } from "react-router-dom";
 import PlaceList from "../components/PlaceList";
 
 const DUMMY_PLACES = [
   {
-      id: "p1",
-      title: "Burj Khalifa",
-      description: "The tallest building in the world.",
-      imageUrl: "https://upload.wikimedia.org/wikipedia/en/thumb/9/93/Burj_Khalifa.jpg/1200px-Burj_Khalifa.jpg",
-      address: "1 Sheikh Mohammed bin Rashid Boulevard, Dubai, United Arab Emirates",
-      location: {
-        lat: 25.1972,
-        long: 55.2744
-      },
-      creator: "u1"
+    id: "p1",
+    title: "Burj Khalifa",
+    description: "The tallest building in the world.",
+    imageUrl: "https://upload.wikimedia.org/wikipedia/en/thumb/9/93/Burj_Khalifa.jpg/1200px-Burj_Khalifa.jpg",
+    address: "1 Sheikh Mohammed bin Rashid Boulevard, Dubai, United Arab Emirates",
+    location: {
+      lat: 25.1972,
+      long: 55.2744
+    },
+    creator: "u1"
   },
   {
     id: "p2",
@@ -26,11 +26,14 @@ const DUMMY_PLACES = [
       long: 55.2744
     },
     creator: "u2"
-}
+  }
 ];
 const UserPlaces = () => {
+  const userId = useParams().userId;
+  const loadedPlaces = DUMMY_PLACES.filter((place) => place.creator === userId);
+
   return (
-    <PlaceList items={DUMMY_PLACES} />
+    <PlaceList items={loadedPlaces} />
   );
 }
 
