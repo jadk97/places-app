d39dd1a53cbe1318e46a41a503f9b1bb5ba8f143
Jad K <jad_dis@hotmail.com>
2020-02-13 03:05:28 -0500

    setup some basic navigation components with their requisite styling

--- src/user/components/UsersList.js
+++ src/user/components/UsersList.js
@@ -1,12 +1,17 @@
 import React from "react";
-import "./UsersList.css";
+
 import UserItem from "./UserItem";
+import Card from "../../shared/components/UIElements/Card";
+import "./UsersList.css";
+
 
 const UsersList = (props) => {
   if (props.items.length === 0) {
     return (
       <div className="center">
-        <h2>No users found.</h2>
+        <Card>
+          <h2>No users found.</h2>
+        </Card>
       </div>
     );
   }
@@ -14,12 +19,12 @@ const UsersList = (props) => {
   return (
     <ul className="users-list">
       {props.items.map((user) => (
-        <UserItem 
-        key={user.id} 
-        id={user.id}
-        image={user.image}
-        name={user.name}
-        placeCount={user.places}
+        <UserItem
+          key={user.id}
+          id={user.id}
+          image={user.image}
+          name={user.name}
+          placeCount={user.places}
         />
       ))}
     </ul>
