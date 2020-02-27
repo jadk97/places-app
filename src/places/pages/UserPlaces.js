f77e07b06d75e698875f6cec6c8f848106bc9a3d
Jad K <jad_dis@hotmail.com>
2020-02-27 04:32:46 -0500

    updated the UserPlaces component to properly fetch a given user's Places from the database

--- src/places/pages/UserPlaces.js
+++ src/places/pages/UserPlaces.js
@@ -1,40 +1,38 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import PlaceList from "../components/PlaceList";
+import { useHttpClient } from "../../shared/hooks/http-hook"
+import ErrorModal from "../../shared/components/UIElements/ErrorModal";
+import LoadingSpinner from "../../shared/components/UIElements/LoadingSpinner";
 
-
-const DUMMY_PLACES = [
-  {
-    id: "p1",
-    title: "Burj Khalifa",
-    description: "The tallest building in the world.",
-    imageUrl: "https://upload.wikimedia.org/wikipedia/en/thumb/9/93/Burj_Khalifa.jpg/1200px-Burj_Khalifa.jpg",
-    address: "1 Sheikh Mohammed bin Rashid Boulevard, Dubai, United Arab Emirates",
-    location: {
-      lat: 25.1972,
-      lng: 55.2744
-    },
-    creator: "u1"
-  },
-  {
-    id: "p2",
-    title: "Burj Khalifa",
-    description: "The tallest building in the world.",
-    imageUrl: "https://upload.wikimedia.org/wikipedia/en/thumb/9/93/Burj_Khalifa.jpg/1200px-Burj_Khalifa.jpg",
-    address: "1 Sheikh Mohammed bin Rashid Boulevard, Dubai, United Arab Emirates",
-    location: {
-      lat: 25.1972,
-      lng: 55.2744
-    },
-    creator: "u2"
-  }
-];
 const UserPlaces = () => {
+  const { isLoading, error, sendRequest, clearError } = useHttpClient();
   const userId = useParams().userId;
-  const loadedPlaces = DUMMY_PLACES.filter((place) => place.creator === userId);
+  const [loadedPlaces, setLoadedPlaces] = useState();
+
+  useEffect(() => {
+    const fetchPlaces = async () => {
+      try {
+        const responseData = await sendRequest(`http://localhost:5000/api/places/user/${userId}`);
+        setLoadedPlaces(responseData.places);
+      }
+      catch (err) { }
+    };
+
+    fetchPlaces();
+  }, [sendRequest, userId])
 
   return (
-    <PlaceList items={loadedPlaces} />
+    <React.Fragment>
+      <ErrorModal error={error} onClear={clearError}/>
+      {isLoading && (
+        <div className="center">
+        <LoadingSpinner />
+        </div>
+      )}
+      {!isLoading && loadedPlaces && <PlaceList items={loadedPlaces} />}
+
+    </React.Fragment>
   );
 }
 
