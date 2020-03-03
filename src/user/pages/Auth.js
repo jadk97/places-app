f17a863ada06b51b988beba2f7fea68f6df6a54a
Jad K <jad_dis@hotmail.com>
2020-03-02 23:29:16 -0500

    implemented image uploading functionality upon Place creation

--- src/user/pages/Auth.js
+++ src/user/pages/Auth.js
@@ -117,7 +117,12 @@ const Auth = () => {
               onInput={inputHandler}
             />}
             {!isLoginMode && (
-              <ImageUpload center id="image" onInput={inputHandler}/>
+              <ImageUpload 
+              center 
+              id="image" 
+              onInput={inputHandler}
+              errorText="Please provide an image."
+              />
             )}
           <Input
             element="input"
