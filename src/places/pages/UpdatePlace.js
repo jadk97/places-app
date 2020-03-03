55b1168cf99dcad3c72aa1a78a4b3c3e555db17b
Jad K <jad_dis@hotmail.com>
2020-03-03 03:22:48 -0500

    modified the URLs in all the requisite components to be ready for deploymnet

--- src/places/pages/UpdatePlace.js
+++ src/places/pages/UpdatePlace.js
@@ -34,7 +34,7 @@ const UpdatePlace = () => {
   useEffect(() => {
     const fetchPlace = async () => {
       try {
-        const responseData = await sendRequest(`http://localhost:5000/api/places/${placeId}`);
+        const responseData = await sendRequest(`${process.env.REACT_APP_BACKEND_URL}/places/${placeId}`);
         setLoadedPlace(responseData.place);
         setFormData({
           title: {
@@ -73,7 +73,7 @@ const UpdatePlace = () => {
   const placeUpdateSubmitHandler = async (event) => {
     event.preventDefault();
     try {
-      await sendRequest(`http://localhost:5000/api/places/${placeId}`,
+      await sendRequest(`${process.env.REACT_APP_BACKEND_URL}/places/${placeId}`,
         'PATCH',
         JSON.stringify({
           title: formState.inputs.title.value,
