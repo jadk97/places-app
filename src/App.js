d37f752623492fd07d0db0e6ce542ce7eb0e7758
Jad K <jad_dis@hotmail.com>
2020-03-03 01:56:18 -0500

    added some basic auto-login functionality

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useCallback } from 'react';
+import React, { useState, useCallback, useEffect } from 'react';
 import { BrowserRouter as Router, Route, Redirect, Switch } from "react-router-dom";
 import Users from "./user/pages/Users";
 import NewPlace from "./places/pages/NewPlace";
@@ -11,16 +11,29 @@ function App() {
   const [token, setToken] = useState(false);
   const [userId, setUserId] = useState(false);
 
+  
   const login = useCallback((uid, token) => {
     setToken(token);
+    localStorage.setItem("userData", JSON.stringify({
+      userId: uid,
+      token
+    }))
     setUserId(uid);
   }, []);
-
+  
   const logout = useCallback(() => {
     setToken(null);
     setUserId(null);
+    localStorage.removeItem("userData");
   }, []);
 
+  useEffect(() => {
+    const storedData = JSON.parse(localStorage.getItem("userData"));
+    if (storedData && storedData.token){
+      login(storedData.userId, storedData.token);
+    }
+  }, [login]);
+  
   let routes;
 
   if (token) {
